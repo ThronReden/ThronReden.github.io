85aa9565345cc5665f8226e8b1421eba084e89f8
Jorge Sánchez <54033978+ThronReden@users.noreply.github.com>
2026-04-29 09:20:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,6 +26,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: "https://decker-theme.pages.dev", // change to your domain
+  site: "https://ThronReden.github.io", // change to your domain
   integrations: [sitemap()]
 });
