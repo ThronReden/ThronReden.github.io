80fd3d3418a7d74dcf7662701523370815a5414e
Jorge <jsanchezsantaliestra@gmail.com>
2026-05-06 14:15:12 +0200

    fix localizacion blog y all drafted

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,7 +10,7 @@ const projects = defineCollection({
         title: z.string().max(50),
         tools: z.preprocess(
             (val) => (Array.isArray(val) ? val : [val]),
-            z.array(z.enum(["TypeScript", "LIVE", "HTML", "JavaScript", "React Native", "Backend API", "Node.js", "Docker", "Tailwind"]))), // change categories here
+            z.array(z.enum(["TypeScript", "Git", "LIVE", "HTML", "Java", "Maven", "MySQL", "JavaScript", "React Native", "Backend API", "Node.js", "Docker", "Tailwind"]))), // change categories here
         year: z.string().max(4),
         liveSite: z.url().optional(),
         github: z.url().optional(),
--- src/content/projects/SudokuKid_ES.md
+++ src/content/projects/SudokuKid_ES.md
@@ -1,12 +1,11 @@
 ---
 id: 1
-lang: en
-title: "Fluxel"
-tools: ["TypeScript", "LIVE"]
-year: "2024"
-github: "https://github.com"
-liveSite: "https://astro.build"
+lang: es
+title: "SudokuKid"
+tools: ["Java", "Git"]
+year: "2026"
+github: "https://github.com/ThronReden/SudokuKid"
 description: "Fluxel is a web tool for converting video into optimized image sequences, designed for high-performance animation workflows. Built with a focus on speed and developer ergonomics, it streamlines assets for modern frontend frameworks and motion libraries."
-isFeatured: false
-isDraft: false
+isFeatured: true
+isDraft: true
 ---
